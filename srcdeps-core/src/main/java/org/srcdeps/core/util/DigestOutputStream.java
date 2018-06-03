e3b1e8f9ccbc6cbd8c26dbe3138537cc48b61f0e
Peter Palaga <ppalaga@redhat.com>
2018-06-03 12:10:41 +0200

    Add BuildRequest hash computation log messages

--- srcdeps-core/src/main/java/org/srcdeps/core/util/DigestOutputStream.java
+++ srcdeps-core/src/main/java/org/srcdeps/core/util/DigestOutputStream.java
@@ -20,12 +20,16 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.security.MessageDigest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * An {@link OutputStream} doing nothing else than computing digest out of the input bytes.
  *
  * @author <a href="https://github.com/ppalaga">Peter Palaga</a>
  */
 public class DigestOutputStream extends OutputStream {
+    private static final Logger log = LoggerFactory.getLogger(DigestOutputStream.class);
     private final MessageDigest digest;
 
     public DigestOutputStream(MessageDigest digest) {
@@ -41,12 +45,26 @@ public class DigestOutputStream extends OutputStream {
     }
 
     @Override
+    public void write(byte[] b) throws IOException {
+        if (log.isDebugEnabled()) {
+            log.debug("srcdeps updates digest with "+ SrcdepsCoreUtils.bytesToHexString(b));
+        }
+        write(b, 0, b.length);
+    }
+
+    @Override
     public void write(byte[] input, int offset, int len) throws IOException {
+        if (log.isDebugEnabled()) {
+            log.debug("srcdeps updates digest with "+ SrcdepsCoreUtils.bytesToHexString(input, offset, len));
+        }
         digest.update(input, offset, len);
     }
 
     @Override
     public void write(int b) throws IOException {
+        if (log.isDebugEnabled()) {
+            log.debug("srcdeps updates digest with "+ SrcdepsCoreUtils.bytesToHexString(new byte[] {(byte) b}));
+        }
         digest.update((byte) b);
     }
 
